1e5f87a822bf95c48bc5bf1a6344d75611517a72
Rui Yang <techoc@foxmail.com>
2025-08-24 21:33:22 +0800

    refactor(leetcode): 优化 T27 解法的代码可读性
    - 为关键步骤添加注释，解释快慢指针的作用
    - 优化变量命名，使代码更易理解

--- src/main/java/cn/techoc/leetcode/easy/T27.java
+++ src/main/java/cn/techoc/leetcode/easy/T27.java
@@ -9,11 +9,16 @@ public class T27 {
      * @return 数组长度
      */
     public int removeElement(int[] nums, int val) {
+        // 初始化快慢指针
         int fast = 0, slow = 0;
+        // 快指针遍历数组
         while (fast < nums.length) {
+            // 慢指针指向的元素不等于val
             if (nums[fast] != val) {
+                // 慢指针后移 与快指针指向的元素相等
                 nums[slow++] = nums[fast];
             }
+            // 快指针后移
             fast++;
         }
         return slow;
